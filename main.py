af814ae08c5fa652d271918d7acddf7e90cab1ee
Ryan-yang125 <1598094295@qq.com>
2020-10-19 18:34:14 +0800

    upload image ans show with flask

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, url_for
+import os
+
+from flask import Flask, render_template, url_for, request, redirect, flash
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -6,6 +9,12 @@ app = Flask(__name__, instance_relative_config=True)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/kmeans')
+def kmeans():
+    import kmeans
+    kmeans.getColor()
 
 
 if __name__ == "__main__":
